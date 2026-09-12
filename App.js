7825d5d4ddf74cdf0fa380172433177691239507
S Bharath Kumar <sbharathk390@gmail.com>
2026-09-12 23:51:03 +0530

    Create App.js with Sample and Counter components

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Sample from './component/Sample';
+import Counter from './component/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <Sample />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
